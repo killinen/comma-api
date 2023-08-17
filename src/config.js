5882ced2a011a3bd18c8f5336b07b12c0c8c1bfc
killinen <37126045+killinen@users.noreply.github.com>
2023-08-17 23:40:48 +0300

    Update config.js
    Change the device stat fetch URL to own server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const COMMA_URL_ROOT = 'https://api.commadotai.com/';
+export const COMMA_URL_ROOT = 'http://192.168.1.150:3000/';
 export const ATHENA_URL_ROOT = 'https://athena.comma.ai/';
 export let VIDEO_HOST = process.env.COMMA_VIDEO_CDN;
 if (!VIDEO_HOST) {
