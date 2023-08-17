26a01a13d99b5f0e35235e566b1baba7351e4147
killinen <37126045+killinen@users.noreply.github.com>
2023-08-17 23:28:11 +0300

    Update config.js
    Change device stat URL to own

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  COMMA_URL_ROOT: window?.COMMA_URL_ROOT || 'https://api.comma.ai/',
+  COMMA_URL_ROOT: window?.COMMA_URL_ROOT || 'http://192.168.1.150:3000/',
   ATHENA_URL_ROOT: window?.ATHENA_URL_ROOT || 'https://athena.comma.ai/',
   BILLING_URL_ROOT: window?.BILLING_URL_ROOT || 'https://billing.comma.ai/',
 };
